433c8ecb05fba440f4737cbf84e1fef3abfdf806
bryanlopezb <bryanlopezb@protonmail.com>
2021-08-09 12:45:18 -0500

    Cats API implemented

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/catfact")
+def catfact():
+
+    url = "https://catfact.ninja/fact"
+    r = requests.get(url)
+    fact = r.json()
+    #print to console
+    print("Did you know?: \n")
+    print(fact)
+    
+
+    return Response(json.dumps(fact))
+
+if __name__ == '__main__':
+    app.run()
