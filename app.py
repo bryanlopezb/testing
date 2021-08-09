cdf52d915b60afd236f201bae9843d63bfe0f383
bryanlopezb <bryanlopezb@protonmail.com>
2021-08-09 12:56:51 -0500

    Yahoo Finance API added

--- app.py
+++ app.py
@@ -14,12 +14,33 @@ def catfact():
     url = "https://catfact.ninja/fact"
     r = requests.get(url)
     fact = r.json()
+    printable_fact = fact['fact']
     #print to console
-    print("Did you know?: \n")
-    print(fact)
-    
-
+    print("Did you know?: " + printable_fact)
     return Response(json.dumps(fact))
+
+@app.route("/get-price/<ticker>")
+def get_price(ticker):
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    response = requests.get(url)
+    company_info = response.json()
+
+    print(company_info)
+
+    price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+    company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+    exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+    currency = company_info['quoteSummary']['result'][0]['price']['currency']
+
+    result = {
+        "price": price,
+        "name": company_name,
+        "exchange": exchange,
+        "currency": currency
+    }
+    print(result)
+
+    return Response(json.dumps(result))
 
 if __name__ == '__main__':
     app.run()
