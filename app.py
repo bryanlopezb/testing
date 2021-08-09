33c72123d5f1f3fa912e48b6270241a2fd2a5a2e
bryanlopezb <bryanlopezb@protonmail.com>
2021-08-09 16:08:21 -0500

    Fixed 403 issue in yahoo finance

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def catfact():
 @app.route("/get-price/<ticker>")
 def get_price(ticker):
     url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
-    response = requests.get(url)
+    headers={'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url,headers=headers)
     company_info = response.json()
 
     print(company_info)
